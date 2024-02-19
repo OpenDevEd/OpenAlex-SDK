923302c57c9a07cd9dad8d494e6f643e6c0fc73c
Omar Aizab <omaraizab98@gmail.com>
2024-02-19 12:31:17 +0100

    update src/entity/work.ts
    add ApcPayment and LocationOpenAlex and Source

--- src/entity/work.ts
+++ src/entity/work.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, PrimaryGeneratedColumn, OneToOne, JoinColumn, Entity } from 'typeorm';
 
 @Entity()
 export class Work {
@@ -12,22 +12,34 @@ export class Work {
   alternate_host_venues?: any;
 }
 
+export type AuthorPosition = 'first' | 'last' | 'middle';
 @Entity()
-class Authorship {
+export class Authorship {
   @PrimaryGeneratedColumn()
   id: string;
 
+  @OneToOne(() => Author)
+  @JoinColumn()
   author: Author;
-  author_position?: 'first' | 'last' | 'middle';
+
+  @Column({ enum: ['first', 'last', 'middle'], nullable: true })
+  author_position?: AuthorPosition;
+
+  @Column({ type: 'simple-array', nullable: true })
   countries?: string[];
+
+  // TODO: ask about Author and Institution relationship
   institutions?: Institution[];
+
   is_corresponding?: boolean;
+
   raw_affiliation_string?: string;
+
   raw_author_name?: string;
 }
 
 @Entity()
-class Author {
+export class Author {
   @Column({ primary: true })
   id: string;
 
@@ -37,9 +49,8 @@ class Author {
   @Column({ nullable: true })
   orcid?: string;
 }
-
 @Entity()
-class Institution {
+export class Institution {
   @Column({ primary: true })
   id: string;
 
@@ -57,4 +68,68 @@ class Institution {
 
   @Column({ type: 'simple-array', nullable: true })
   lineage?: string[];
+}
+
+@Entity()
+export class ApcPayment {
+  @Column({ nullable: true })
+  value?: number;
+
+  @Column({ nullable: true })
+  currency?: string;
+
+  @Column({ nullable: true })
+  provenance?: string;
+
+  @Column({ nullable: true })
+  value_usd?: number;
+}
+
+@Entity()
+export class LocationOpenAlex {
+  @Column({ nullable: true })
+  is_accepted?: boolean;
+
+  @Column({ nullable: true })
+  is_oa?: boolean;
+
+  @Column({ nullable: true })
+  is_published?: boolean;
+
+  @Column({ nullable: true })
+  landing_page_url?: string;
+
+  @Column({ nullable: true })
+  license?: string;
+
+  @OneToOne(() => Source)
+  @JoinColumn()
+  source?: Source;
+
+  @Column({ nullable: true })
+  pdf_url?: string;
+
+  @Column({ nullable: true })
+  version?: string;
+}
+
+@Entity()
+export class Source {
+  @Column({ primary: true })
+  id: string;
+
+  @Column({ nullable: true })
+  display_name?: string;
+
+  @Column({ nullable: true })
+  issn_l?: string;
+
+  @Column({ type: 'simple-array', nullable: true })
+  issn?: string[];
+
+  @Column({ nullable: true })
+  host_organization?: string;
+
+  @Column({ nullable: true })
+  type?: string;
 }
