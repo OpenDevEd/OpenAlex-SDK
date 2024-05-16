f33798e8b91716a8a1e481fe83f1786a9afc87eb
Hassan Mansour <hasana13work@gmail.com>
2024-05-16 14:41:12 +0100

    updated snowballing to work with recursion

--- src/utils/snowBalling.ts
+++ src/utils/snowBalling.ts
@@ -1,21 +1,29 @@
+import fs from 'fs';
 import { ExternalIdsWork, Work } from 'src/types/work';
 import OpenAlex from '../index';
+
+type WorkWithChildren = {
+  work: Work;
+  children: WorkWithChildren[];
+};
+
+type SnowballingResult = {
+  citedByResults: WorkWithChildren[];
+  citesResults: WorkWithChildren[];
+  relatedResults: WorkWithChildren[];
+};
 
 async function snowBalling(
   id: string,
   externalIds?: ExternalIdsWork,
-  // level: number = 1,
-): Promise<{
-  citedByResults: Work[];
-  citesResults: Work[];
-  relatedResults: Work[];
-}> {
+  level: number = 1,
+): Promise<SnowballingResult> {
   const openAlex = new OpenAlex();
   const work = await openAlex.work(id, externalIds);
 
-  let citedByResults: any[] = [];
-  const citesResults: any[] = [];
-  const relatedResults: any[] = [];
+  const citedByResults: WorkWithChildren[] = [];
+  const citesResults: WorkWithChildren[] = [];
+  const relatedResults: WorkWithChildren[] = [];
 
   console.log(`Starting snowballing for ${work.title}`);
   console.log(`extracting cited by works for ${work.title}`);
@@ -26,36 +34,53 @@ async function snowBalling(
       cites: id,
     },
   });
-  // add the results to the citedByResults
-  citedByResults = citedByResults.concat(works.results);
-
+  for (const result of works.results) {
+    citedByResults.push({
+      work: result,
+      children:
+        level > 1
+          ? (await snowBalling(result.id, undefined, level - 1)).citedByResults
+          : [],
+    });
+  }
   console.log(
-    `extracted ${works.results.length} cited by works for ${work.title}`,
+    `extracted ${citedByResults.length} cited by works for ${work.title}`,
   );
 
   // extract cites works
-  // loop through the references and get the work
   console.log(`extracting cites works for ${work.title}`);
-
   if (work.referenced_works) {
     for (const reference of work.referenced_works) {
-      const work = await openAlex.work(reference);
-      citesResults.push(work);
+      const referencedWork = await openAlex.work(reference);
+      citesResults.push({
+        work: referencedWork,
+        children:
+          level > 1
+            ? (await snowBalling(reference, undefined, level - 1)).citesResults
+            : [],
+      });
     }
   }
   console.log(`extracted ${citesResults.length} cites works for ${work.title}`);
+
   // extract related works
-  // loop through the related works and get the work
   console.log(`extracting related works for ${work.title}`);
   if (work.related_works) {
     for (const related of work.related_works) {
-      const work = await openAlex.work(related);
-      relatedResults.push(work);
+      const relatedWork = await openAlex.work(related);
+      relatedResults.push({
+        work: relatedWork,
+        children:
+          level > 1
+            ? (await snowBalling(related, undefined, level - 1)).relatedResults
+            : [],
+      });
     }
   }
   console.log(
     `extracted ${relatedResults.length} related works for ${work.title}`,
   );
+
   return {
     citedByResults,
     citesResults,
@@ -63,7 +88,8 @@ async function snowBalling(
   };
 }
 (async () => {
-  const res = await snowBalling('W2741809807');
+  const res = await snowBalling('W4377994187', undefined, 2);
+  fs.writeFileSync('snowballing.json', JSON.stringify(res, null, 2));
   console.log('citedByResults length: ', res.citedByResults.length);
   console.log('citesResults length: ', res.citesResults.length);
   console.log('relatedResults length: ', res.relatedResults.length);
